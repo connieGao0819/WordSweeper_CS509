a030fc8609bd846f2bb7b43094ea18e3ac48f18c
connieGao0819 <jgao4@wpi.edu>
2016-11-07 18:33:29 -0500

    Update RandomLetter.java

--- word-sweeper/src/com/wpi/service/RandomLetter.java
+++ word-sweeper/src/com/wpi/service/RandomLetter.java
@@ -6,14 +6,14 @@ public class RandomLetter {
     
     private static final String[] LETTER_SET={"A",
             "B","C","D","E","F","G","H","I","J","K",
-            "L","M","N","O","P","Q","R","S","T","U",
+            "L","M","N","O","P","R","S","T","U",
             "V","W","X","Y","Z","Qu"};
     
     public String[] getRandomLetter(int len){
         String[] letterArray = new String[len];
         Random random = new Random();
         for(int i=0; i<len; i++){
-            letterArray[i] = LETTER_SET[random.nextInt(26)+1];
+            letterArray[i] = LETTER_SET[random.nextInt (26)];
         }
         return letterArray;
     }
